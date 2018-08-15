fdb29fdbd34a41039bd80c68d2d338fd26bd7111
tymoschuk <tymoschuk@A34COMP8>
2018-08-15 15:25:04 +0300

    add some checks

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -37,6 +38,11 @@ namespace WpfApp1
             
 
         }
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
         int count = 1;
         private void Button_Click(object sender, RoutedEventArgs e)
         {
@@ -45,9 +51,10 @@ namespace WpfApp1
             {
                 if (int.Parse(tbCode.Text) == int.Parse(dataTable.Rows[i][0].ToString()))
                 {
+                  
                     totalprice += double.Parse(dataTable.Rows[i][2].ToString())*int.Parse(tbAmount.Text);
-                    listbox.Items.Add(count+"   "+dataTable.Rows[i][0] + "   " + dataTable.Rows[i][1] + "\n" +tbAmount.Text +"x"+string.Format("{0:C}",
-                        double.Parse( dataTable.Rows[i][2].ToString()))+'\n'+ string.Format("{0:C}", totalprice));
+                    listbox.Items.Add(count + ")   " + dataTable.Rows[i][1] + "\n" +tbAmount.Text +"x"+string.Format("{0:C}",
+                        double.Parse( dataTable.Rows[i][2].ToString()))+"\n---------------------\n"+ string.Format("{0:C}", int.Parse(tbAmount.Text) * double.Parse(dataTable.Rows[i][2].ToString())));
                     
                     lblPrice.Content = string.Format("{0:C}", totalprice) ;
                     tbAmount.Text = "1";
